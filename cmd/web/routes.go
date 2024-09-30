3b2a97ce9bfcee8a1c5dc9b67a0fb799c4fe191c
Autumn716 <2683488768@QQ.com>
2024-09-30 19:52:13 +0800

    befor P171 chapter  6.3. was finished

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,13 +2,16 @@ package main
 
 import "net/http"
 
-// The routes() method returns a servemux containing our application routes.
-func (app *application) routes() *http.ServeMux {
+// Update in P168
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	return mux
+
+	return secureHeaders(mux)
 }
