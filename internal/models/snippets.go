d3064917be5a258f71ca188515bdd775499dd0d3
Autumn716 <2683488768@QQ.com>
2024-09-30 15:07:26 +0800

    before P116 chapter 4.8. was finfished

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -20,12 +21,45 @@ type SnippetModel struct {
 
 // This will insert a new snippet into the database.
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-	return 0, nil
+	// 编写 SQL语句，使用``来将语句分为两行，便于阅读和使用
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+ 	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	// 在嵌入的连接池中使用 DB的Exec方法 来执行SQL语句
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	//	在result上使用 LastInsertId 方法，获得 snippets 表的最新插入记录
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	// 将int64的id 转换成int类型再返回
+	return int(id), nil
 }
 
 // This will return a specific snippet based on its id.
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+ 	WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	row := m.DB.QueryRow(stmt, id)
+
+	s := &Snippet{}
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	return s, nil
 }
 
 // This will return the 10 most recently created snippets.
