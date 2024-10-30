b3812f2d0edd334db8b508181380cab8c0a3a580
Autumn716 <2683488768@QQ.com>
2024-10-30 23:10:20 +0800

    before P269 chapter 11.3. was finished.

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -2,4 +2,10 @@ package models
 
 import "errors"
 
-var ErrNoRecord = errors.New("models: no matching record found")
+var (
+	ErrNoRecord = errors.New("models: no matching record found")
+	//if a user tries to login with an incorrect email address or password.
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	//if a user tries to signup with an email address that's already in use.
+	ErrDuplicateEmail = errors.New("models: duplicate email")
+)
