594a83f03503691c5ea0287d0ece030f52103cac
Autumn716 <2683488768@QQ.com>
2024-11-03 20:51:16 +0800

    before P293 chapter 11.5. was finished

--- internal/models/users.go
+++ internal/models/users.go
@@ -45,14 +45,37 @@ func (m *UserModel) Insert(name, email, password string) error {
 		}
 		return err
 	}
-	
+
 	return nil
 }
 
 // Authenticate method to verify whether a user exists with the provided email address and password.
 // return the relevant user ID
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	var id int
+	var hashedPassword []byte
+
+	stmt := "SELECT id, hashed_password FROM users WHERE email = ?"
+
+	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	return id, nil
 }
 
 // Exists method to check if a user exists with a specific ID.
