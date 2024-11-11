5e04074ffd67870d65fc1e1cd3417c774cfcd393
Autumn716 <2683488768@QQ.com>
2024-11-11 15:15:17 +0800

    before P370 chapter 14.6. was finished fix(testuitls_test.go) snippets and users field test end-to-end about  SnippetView handler

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/Autumn716/snippetbox/internal/models"
+	"github.com/Autumn716/snippetbox/internal/models/mocks"
 	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
 )
@@ -30,8 +30,8 @@ func newTestApplication(t *testing.T) *application {
 	return &application{
 		errorLog:       log.New(io.Discard, "", 0),
 		infoLog:        log.New(io.Discard, "", 0),
-		snippets:       &models.SnippetModel{},
-		users:          &models.UserModel{},
+		snippets:       &mocks.SnippetModel{},
+		users:          &mocks.UserModel{},
 		templateCache:  templateCache,
 		formDecoder:    formDecoder,
 		sessionManager: sessionManager,
